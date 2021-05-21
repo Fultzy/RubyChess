904eb6ad99fbc7d802e448636db9515a1ae5e658
big <fultyz92@gmail.com>
2021-05-21 14:08:04 -0500

    origin, create-a-kight assignment

--- rubychess.rb
+++ rubychess.rb
@@ -1,5 +1,6 @@
 ## Ruby chess created by Fultzy! :D
-## ruby 2.6.2
+## ruby-v: 2.6.2
+## likely using ruby2d-v: 0.9.5
 
 require './lib/gamestate.rb'
 require './lib/player.rb'
@@ -7,4 +8,11 @@ require './lib/computer.rb'
 require './lib/gamestate.rb'
 require './lib/pieces.rb'
 
+# board size 8x8 
+
+# single player
+# create game state & Board
+game = Gamestate.new(player)
+
+# create a knight and display moves
 knight = Knight.new(white, ())
