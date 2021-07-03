833a662e5b7ad554afc71d596e68753d7b864411
big <fultyz92@gmail.com>
2021-07-03 13:21:20 -0500

    Built out King w/tests

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -8,8 +8,20 @@ class King
     @move_count = 0
   end
 
-  def list_moves(_location = @location)
+  # The king can move in all dirrections but only one
+  # space at a time. @move_counter` exists for castling.
+  # castling is a specal move, when neither the king and left/
+  # right side rook have moved and the spaces between are
+  # free, youre allowed to move the king two spaces towords
+  # one of the rooks and place that rook on the other side of king.
 
+
+  def list_moves(_location = @location, list = [])
+    move_mod = [[-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1]]
+    move_mod.each do |mod|
+      list.push([location[0] + mod[0], location[1] + mod[1]])
+    end
+    list
   end
 
   def move(new_location)
